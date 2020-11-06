d4a9414b398638fdedc341d91856a2991af412a5
James Sousa <jameswsousa.eng@gmail.com>
2020-11-06 14:28:48 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:favoritos_youtube/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterTube',
+    
+      home: Home(),
+    );
+  }
+}
+
+
+
+
+
+ 
