0e0bc9e7c1d782fa42597c79d9bc2ce416c2b856
James Sousa <jameswsousa.eng@gmail.com>
2020-11-13 19:26:55 -0400

    builds home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,41 @@
+import 'package:favoritos_youtube/delegates/data_search.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        elevation: 0,
+        title: Container(
+            height: 50,
+            width: 120,
+            child: Image.asset(
+              'assets/youtube.png',
+              fit: BoxFit.fitWidth,
+            )),
+        actions: [
+         
+          Container(child: Text("0",), alignment: Alignment.center,),
+          IconButton(
+              icon: Icon(
+                Icons.star,
+                color: Colors.white,
+              ),
+              onPressed: () {}),IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () async {
+                String result= await showSearch(context: context, delegate: DataSearch());
+                print(result);
+              }),
+        ],
+      ),
+      body: Container(),
     );
   }
 }
