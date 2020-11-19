92062733fe9e548a86cfb316182f2b60c25c109e
James Sousa <jameswsousa.eng@gmail.com>
2020-11-19 15:34:29 -0400

    creates 'favorite' local saver

--- lib/blocs/favorite_bloc.dart
+++ lib/blocs/favorite_bloc.dart
@@ -1,13 +1,26 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:favoritos_youtube/models/video.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoriteBloc implements BlocBase {
   Map<String, Video> _favorites = {};
   final StreamController<Map<String, Video>> _favController =
       StreamController<Map<String, Video>>.broadcast();
   Stream<Map<String, Video>> get outFav => _favController.stream;
+
+  FavoriteBloc() {
+    SharedPreferences.getInstance().then((prefs) {
+      if (prefs.getKeys().contains('favorites')) {
+        _favorites = json.decode(prefs.getString('favorites')).map((k, v) {
+          return MapEntry(k, Video.fromJson(v));
+        }).cast<String, Video>();
+        _favController.add(_favorites);
+      }
+    });
+  }
 
   void togglefavorite(Video video) {
     if (_favorites.containsKey(video.id))
@@ -16,6 +29,13 @@ class FavoriteBloc implements BlocBase {
       _favorites[video.id] = video;
 
     _favController.sink.add(_favorites);
+    _saveFav();
+  }
+
+  void _saveFav() {
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setString('favorites', json.encode(_favorites));
+    });
   }
 
   @override
