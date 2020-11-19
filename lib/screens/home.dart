dd0059368c6ff1a5eae6a78b42dcea6b683c7657
James Sousa <jameswsousa.eng@gmail.com>
2020-11-19 13:54:14 -0400

    creates functioning home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,14 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:favoritos_youtube/blocs/videos_bloc.dart';
 import 'package:favoritos_youtube/delegates/data_search.dart';
+import 'package:favoritos_youtube/widgets/video_tile.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black87,
         elevation: 0,
@@ -17,25 +20,46 @@ class Home extends StatelessWidget {
               fit: BoxFit.fitWidth,
             )),
         actions: [
-         
-          Container(child: Text("0",), alignment: Alignment.center,),
+          Container(
+            child: Text(
+              "0",
+            ),
+            alignment: Alignment.center,
+          ),
           IconButton(
               icon: Icon(
                 Icons.star,
                 color: Colors.white,
               ),
-              onPressed: () {}),IconButton(
+              onPressed: () {}),
+          IconButton(
               icon: Icon(
                 Icons.search,
                 color: Colors.white,
               ),
               onPressed: () async {
-                String result= await showSearch(context: context, delegate: DataSearch());
-                print(result);
+                String result =
+                    await showSearch(context: context, delegate: DataSearch());
+                if (result != null)
+                  BlocProvider.getBloc<VideoBloc>().inSearch.add(result);
               }),
         ],
       ),
-      body: Container(),
+      body: StreamBuilder(
+        stream: BlocProvider.getBloc<VideoBloc>().outVideos,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) { 
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return VideoTile(snapshot.data[index]);
+              },
+              itemCount: snapshot.data.length,
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
